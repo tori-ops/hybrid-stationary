0cbaff25fa205057ea05a404a2eab0c16baaf6dc
Tori <tori@missingpieceplanning.com>
2026-01-13 19:55:40 -0500

    Increase timeline image to w-80 h-80 with object-contain to show full image

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -142,7 +142,7 @@ function InvitePageContent() {
                   <img 
                     src={invitation.timeline_image_url}
                     alt="Timeline decoration"
-                    className="w-40 h-40 rounded-full object-cover"
+                    className="w-80 h-80 rounded-full object-contain"
                   />
                 </div>
               )}
